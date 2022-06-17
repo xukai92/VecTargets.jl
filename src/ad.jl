c87b7e0bbf9ca9af2089842599439368edd4e9ff
Kai Xu <me@xuk.ai>
2022-06-18 00:08:56 +0100

    feat: compiled Hessian via ReverseDiff

--- src/ad.jl
+++ src/ad.jl
@@ -1,14 +1,12 @@
 using ReverseDiff: ReverseDiff, DiffResults
 
 function gen_grad(func, x::AbstractVector)
-    inputs = (x,)
-    f_tape = ReverseDiff.GradientTape(func, inputs)
+    f_tape = ReverseDiff.GradientTape(func, x)
     compiled_f_tape = ReverseDiff.compile(f_tape)
-    results = similar.(inputs)
-    all_results = DiffResults.GradientResult.(results)
+    result = DiffResults.GradientResult(x)
     function grad(x::AbstractVector)
-        ReverseDiff.gradient!(all_results, compiled_f_tape, (x,))
-        return DiffResults.value(first(all_results)), DiffResults.gradient(first(all_results))
+        ReverseDiff.gradient!(result, compiled_f_tape, x)
+        return DiffResults.value(result), DiffResults.gradient(result)
     end
     return grad
 end
@@ -19,18 +17,43 @@ function gen_grad(func, x::AbstractMatrix)
         retval = func(x)
         return sum(retval)
     end
-    inputs = (x,)
-    f_tape = ReverseDiff.GradientTape(retval_sum, inputs)
+    f_tape = ReverseDiff.GradientTape(retval_sum, x)
     compiled_f_tape = ReverseDiff.compile(f_tape)
-    results = similar.(inputs)
-    all_results = DiffResults.GradientResult.(results)
+    result = DiffResults.GradientResult(x)
     function grad(x::AbstractMatrix)
-        ReverseDiff.gradient!(all_results, compiled_f_tape, (x,))
-        return ReverseDiff.value(retval), DiffResults.gradient(first(all_results))
+        ReverseDiff.gradient!(result, compiled_f_tape, x)
+        return ReverseDiff.value(retval), DiffResults.gradient(result)
     end
     return grad
 end
 
-# Use ReverseDiff to compile gradient function
+function gen_hess(func, x::AbstractVector)
+    f_tape = ReverseDiff.HessianTape(func, x)
+    compiled_f_tape = ReverseDiff.compile(f_tape)
+    result = DiffResults.HessianResult(x)
+    function hess(x::AbstractVector)
+        ReverseDiff.hessian!(result, compiled_f_tape, x)
+        return DiffResults.value(result), DiffResults.gradient(result), DiffResults.hessian(result)
+    end
+    return hess
+end
+
+function gen_hess(func, X::AbstractMatrix)
+    x = first(eachcol(X))
+    f_tape = ReverseDiff.GradientTape(func, x)
+    compiled_f_tape = ReverseDiff.compile(f_tape)
+    result = DiffResults.HessianResult(x)
+    function hess(X::AbstractMatrix)
+        mapreduce(combine_hess_results, eachcol(X)) do x
+            ReverseDiff.gradient!(result, compiled_f_tape, x)
+            DiffResults.value(result), DiffResults.gradient(result), DiffResults.hessian(result)
+        end
+    end
+    return hess
+end
+
+# Use ReverseDiff to compile gradient and hessian function
 gen_logpdf_grad(target, x) = gen_grad(_x -> logpdf(target, _x), x)
-logpdf_grad(target, x) = gen_grad(_x -> logpdf(target, _x), x)(x)
+    logpdf_grad(target, x) = gen_logpdf_grad(target, x)(x)
+gen_logpdf_hess(target, x) = gen_hess(_x -> logpdf(target, _x), x)
+    logpdf_hess(target, x) = gen_logpdf_hess(target, x)(x)
